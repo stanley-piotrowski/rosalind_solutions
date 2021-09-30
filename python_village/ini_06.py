30e51babf92b0a528b3315cc26d81aa02d052115
Stanley Piotrowski <stanleyjpiotrowski@gmail.com>
2021-09-30 16:38:09 -0700

    First correct solution; added example text file from the Rosalind site

---
+++ python_village/ini_06.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+###################
+#### ini_06.py ####
+###################
+
+# Give a string "s" of length at most 10,000 letters, return the number of occurrences of each word in "s" separated by spaces, where order does not matter
+
+# To run this program, supply an input file and type the following:
+# ./ini_06.py <input>
+
+# To get help on this program, type one of the following:
+# ./ini_06.py -h OR ./ini_06.py --help
+
+# For example, given the following string as input: We tried list and we tried dicts also we tried Zen
+# Should return:
+# and 1
+# We 1
+# tried 3
+# dicts 1
+# list 1
+# we 2
+# also 1
+# Zen 1
+
+##############
+#### Code ####
+##############
+
+# Initialize argparse
+import argparse
+parser = argparse.ArgumentParser()
+
+# Define argument for input file
+parser.add_argument("input", type = str, help = "Input file containing string (e.g., input_ini_06.txt)")
+
+# Execute the parser
+args = parser.parse_args()
+
+# Open file handle and push the line to a list
+input = args.input
+input_handle = open(input, "r")
+input_string = input_handle.readline().strip("\n").split(" ")
+
+# Loop through each element in the list and count the number of occurrences
+# Initialize a dictionary, push the string as a key and the number of occurrences as a value
+string_dict = {}
+for string in input_string:
+	string_dict[string] = input_string.count(string)
+
+# Loop over the dictionary items and print the key:value pairs
+for key, value in string_dict.items():
+	print(key, value, sep = " ")
+
